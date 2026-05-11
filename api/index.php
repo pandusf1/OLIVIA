fdfb6a0d29de754b436816278acc6e00d601550e
pandusf1 <pandusf1@gmail.com>
2026-05-12 01:10:28 +0700

    permulaan

--- api/index.php
+++ api/index.php
@@ -1,17 +1,9 @@
 <?php
 // api/index.php
 
-// Buat folder sementara untuk Laravel agar tidak error saat menulis view/cache
-$storageFolders = [
-    '/tmp/storage/framework/views',
-    '/tmp/storage/framework/cache',
-    '/tmp/storage/framework/sessions',
-];
-
-foreach ($storageFolders as $folder) {
-    if (!is_dir($folder)) {
-        mkdir($folder, 0755, true);
-    }
+// Pastikan folder storage dasar ada di /tmp
+if (!is_dir('/tmp/storage/framework/views')) {
+    mkdir('/tmp/storage/framework/views', 0755, true);
 }
 
 require __DIR__ . '/../public/index.php';
